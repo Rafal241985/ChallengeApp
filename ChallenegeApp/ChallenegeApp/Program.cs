5fe714edd526aed11e38872e15df6c3091738794
Rafal241985 <malkiewicz.rafal85@gmail.co>
2023-01-29 21:22:48 +0100

    Praca domowa dzień 4

---
+++ ChallenegeApp/ChallenegeApp/Program.cs
@@ -0,0 +1,16 @@
+﻿var name = "Ewa";
+bool isFemale = true;
+var age = 29;
+
+if (isFemale && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name =="Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if(!isFemale && age<18)
+{
+    Console.WriteLine("Meżczyzna niepełnoletni");
+}
