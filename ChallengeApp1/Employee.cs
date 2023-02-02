cdd20a527492209556a09bc2b0b04f37a1a8076e
Rafal241985 <malkiewicz.rafal85@gmail.co>
2023-02-02 21:47:09 +0100

    Praca nr 6

---
+++ ChallengeApp1/Employee.cs
@@ -0,0 +1,37 @@
+﻿
+namespace ChallengeApp1
+{
+    internal class Employee
+    {
+
+        private List<int> points = new List<int>();
+
+        public string name { get; private set; }
+
+        public string surname { get; private set; }
+
+        public string age { get; private set; }
+
+        public int Points
+        {
+            get
+            {
+                return this.points.Sum();
+            }
+        }
+
+        public Employee(string name, string surname, string age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+
+        }
+
+        public void AddPoints(int point)
+        {
+            this.points.Add(point);
+        }
+    }
+}
+
---
+++ ChallengeApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using ChallengeApp1;
+
+Employee employee1 = new Employee("Rafał", "Małkiewicz", "37");
+Employee employee2 = new Employee("Paweł", "Kowalski", "45");
+Employee employee3 = new Employee("Bartek", "Wrona", "25");
+
+employee1.AddPoints(4);
+employee1.AddPoints(4);
+employee1.AddPoints(4);
+employee1.AddPoints(4);
+
+employee2.AddPoints(5);
+employee2.AddPoints(5);
+employee2.AddPoints(5);
+employee2.AddPoints(5);
+
+employee3.AddPoints(3);
+employee3.AddPoints(3);
+employee3.AddPoints(3);
+employee3.AddPoints(3);
+
+int maxPoints = -1;
+Employee employeeWithMaxResult = null;
+
+List<Employee> employees = new List<Employee>()
+{
+    employee1, employee2, employee3
+};
+
+foreach (var employee in employees)
+{
+    if(employee.Points > maxPoints)
+    {
+        maxPoints= employee.Points;
+        employeeWithMaxResult = employee;
+    }
+}
+
+Console.WriteLine($"Najlepszym pracownikiem został: {employeeWithMaxResult.name} {employeeWithMaxResult.surname}" + $" otrzymał {employeeWithMaxResult.Points} punkty");
